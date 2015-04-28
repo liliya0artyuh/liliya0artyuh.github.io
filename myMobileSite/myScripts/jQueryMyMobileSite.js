2c44ed33db1d2aa9a31c0dcffafda4c341b158d0
liliya0artyuh <liliya0artyukh@gmail.com>
2015-04-28 08:43:52 -0400

    merge with my website on centennial server

--- myMobileSite/myScripts/jQueryMyMobileSite.js
+++ myMobileSite/myScripts/jQueryMyMobileSite.js
@@ -1,11 +1,9 @@
-	// file name: sliderCSS.cs,  author: liliya artyukh, Nikolaj Dam Larsen, Portfolio Mobile website, 5 pages mobile web app
-// file contains javaScript code for the Portfolio Mobile website and slider
+	/* file name: jQueryMyMobileSite.js,  author: liliya artyukh, Nikolaj Dam Larsen, Portfolio Mobile website, 5 pages mobile web app
+ file contains javaScript code for the Portfolio Mobile website and slider */
 
-	function doChange(newPage) {
-      $("body").pagecontainer("change", newPage, {transition: "slide"});
-  	}
 
-  	//Sider Settings
+function main(){
+  	/*Sider Settings*/
 	$(function () {
 	    $("#slider").excoloSlider({
 	        autoPlay: true,
@@ -18,4 +16,39 @@
 	        delay: 0,
 	        interval: 5000
 	    });
+});
+
+$("#visaImg").on("tap",function(){
+  $("#visaDetailsSlider").popup("open");
+});
+
+$("#nemImg").on("tap",function(){
+  $("#nemWebDetailsSlider").popup("open");
+});
+
+$("#csharpImage").on("tap",function(){
+  $("#csharpDetailsSlider").popup("open");
+});
+
+$("#gameImage").on("tap",function(){
+  $("#gameDetailsSlider").popup("open");
+});
+
+        $(document).on('pagecreate', function (evt) {
+            $("#myContent").on("tap", function (e) {
+                alert("you tapped!");
+            });
+
+            $("#myContent").on("swiperight", function (e) {
+                alert("you swiped right!");
+            });
+
+            $("#myContent").on("swipeleft", function (e) {
+                alert("you swiped left!");
+            });
+        });
+}
+
+$(document).bind('mobileinit',function(){
+	$.mobile.page.prototype.options.keepNative = "select, input.foo, textarea.bar";
 });
